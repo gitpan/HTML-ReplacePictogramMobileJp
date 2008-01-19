f13cdc4c60267517fa58a8ff6b0570c0b3c96a8b
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2008-01-19 16:13:17 +0000

    Import of TOKUHIROM/HTML-ReplacePictogramMobileJp-0.03 from CPAN.
    gitpan-cpan-distribution: HTML-ReplacePictogramMobileJp
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         TOKUHIROM/HTML-ReplacePictogramMobileJp-0.03.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

--- lib/HTML/ReplacePictogramMobileJp/Base.pm
+++ lib/HTML/ReplacePictogramMobileJp/Base.pm
@@ -2,21 +2,11 @@ package HTML::ReplacePictogramMobileJp::Base;
 use strict;
 use warnings;
 use base 'Exporter';
-our @EXPORT = qw/validate_args unicode_property unicode_hex_cref filter/;
+our @EXPORT = qw/unicode_property unicode_hex_cref filter img_localsrc/;
 use Params::Validate ':all';
 use Encode;
 use Encode::JP::Mobile ':props';
-
-sub validate_args {
-    validate(
-        @_,
-        +{
-            callback => { type => CODEREF },
-            html     => { type => SCALAR },
-        }
-    );
-    @_;
-}
+use File::ShareDir 'dist_file';
 
 my $property_for = +{
     E => 'InKDDIPictograms',
@@ -33,13 +23,41 @@ sub unicode_hex_cref {
     $_ =~ s/&#x([A-F0-9]{4});/callback(hex $1, $carrier)/ge;
 }
 
+sub _kddi_number2unicode_auto {
+    my $number = shift;
+
+    my $fname =
+      File::ShareDir::dist_file( 'Encode-JP-Mobile', 'kddi-table.pl' );
+    my $dat = do $fname;
+    for my $row (@$dat) {
+        if ( $row->{number} == $number ) {
+            return hex $row->{unicode_auto};
+        }
+    }
+    return;    # invalid number
+}
+
+sub img_localsrc {
+    $_ =~ s{<img[^<>]+localsrc=["'](\d+)[^<>]+>}{
+        callback(_kddi_number2unicode_auto($1), 'E');
+    }ge;
+}
+
 sub filter {
     my ($charset, $decode_by, $code) = @_;
     my $pkg = caller(0);
     no strict 'refs';
     *{"$pkg\::$charset"} = sub {
         my $class = shift;
-        my %args = validate_args(@_);
+        validate(
+            @_,
+            +{
+                callback => { type => CODEREF },
+                html     => { type => SCALAR },
+            }
+        );
+        my %args = @_;
+
         local $_ = decode($decode_by, $args{html}, Encode::FB_XMLCREF);
         local *HTML::ReplacePictogramMobileJp::Base::callback = $args{callback};
         local *{"$pkg\::callback"} = $args{callback};
